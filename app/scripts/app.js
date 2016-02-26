5a82e11b02552307ed0880c90363d628215a9c25
jake giberson <jacob@isodevelopers.com>
2016-02-26 15:36:43 +0000

    all working

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,47 @@
-(function() {
+(function () {
+    var everlive = new Everlive("slwplagd3hs9ib4y");
     document.addEventListener("deviceready", function () {
-        var app = new kendo.mobile.Application(document.body, { skin: "nova" });
+        window.listview = kendo.observable({
+            addImage: function () {
+                var success = function (data) {
+                    everlive.Files.create({
+                        Filename: Math.random().toString(36).substring(2, 15) + ".jpg",
+                        ContentType: "image/jpeg",
+                        base64: data
+                    }).then(loadPhotos);
+                };
+                var error = function () {
+                    navigator.notification.alert("Unfortunately we could not add the image");
+                };
+                var config = {
+                    destinationType: Camera.DestinationType.DATA_URL,
+                    targetHeight: 400,
+                    targetWidth: 400
+                };
+                navigator.camera.getPicture(success, error, config);
+            }
+        });
+        var app = new kendo.mobile.Application(document.body, {
+            skin: "nova"
+        });
+
+        function loadPhotos() {
+            everlive.Files.get().then(function (data) {
+                var files = [];
+                for (var i = data.result.length - 1; i >= 0; i--) {
+                    var image = data.result[i];
+                    files.push(image.Uri);
+                }
+                $("#images").kendoMobileListView({
+                    dataSource: files,
+                    template: "<img src='#: data #'>"
+                });
+            });
+        }
+        loadPhotos();
+
+
         navigator.splashscreen.hide();
     });
+
 }());
